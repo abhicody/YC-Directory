7dfee1d63a14fbfd0b357ebb3b5ea593e34b198d
abhicody <honeydude009@gmail.com>
2024-11-26 13:01:05 +0530

    sentry implementation

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://a764d6aa1d66b9258b4cb094becdcb8e@o4507537249927168.ingest.de.sentry.io/4507537257922640",
+  dsn: "https://f29c0c49014221aae1b3c61a1017631e@o4507809043972096.ingest.us.sentry.io/4507810754658304",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
